d5597c451a01f9edb744029af1dc134950153350
kabyfunfit <100882507+kabyfunfit@users.noreply.github.com>
2024-10-16 17:22:11 -0700

    Update appwrite-client.ts

--- src/lib/appwrite-client.ts
+++ src/lib/appwrite-client.ts
@@ -1,9 +1,15 @@
 import { Client, Account, Databases, ID } from "appwrite";
-import env from "@/config/env";
+
+const endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT;
+const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID;
+
+if (!endpoint || !projectId) {
+  throw new Error('Appwrite endpoint or project ID is not defined');
+}
 
 const client = new Client()
-  .setEndpoint(env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
-  .setProject(env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
+  .setEndpoint(endpoint)
+  .setProject(projectId);
 
 const account = new Account(client);
 const databases = new Databases(client);
